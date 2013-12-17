e72367d0efb5ee3ed8627b5a86f7e81aedb11386
oliverpirez <oliver.garcia@ranchodigital.com.mx>
2013-12-17 13:55:58 -0600

    cambios chuy

--- www/js/BD.js
+++ www/js/BD.js
@@ -14,22 +14,41 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		tx.executeSql("DROP TABLE IF EXISTS prueba;");
 		tx.executeSql("DROP TABLE IF EXISTS fase_lunar;");
 		tx.executeSql("DROP TABLE IF EXISTS eclipse;");
+		tx.executeSql("DROP TABLE IF EXISTS decreto;");
+		tx.executeSql("DROP TABLE IF EXISTS lfc;");
 		
         tx.executeSql( 'CREATE TABLE prueba ("fecha" DATETIME, "sol_signo" VARCHAR, "horario_verano" VARCHAR, estacion VARCHAR, aspecto_astrologico VARCHAR, "luna_signo" VARCHAR, "decreto" VARCHAR, "id_prueba" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );');
 		tx.executeSql( 'CREATE TABLE fase_lunar ("fecha_inicio" DATETIME,  "fecha_fin" DATETIME ,"nombre_fase" VARCHAR,"signo_imagen" VARCHAR, "signo_descripcion" VARCHAR, "aspecto_astrologico" VARCHAR, "num_lunacion" VARCHAR, "id_fase_lunar" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );');
 		tx.executeSql( 'CREATE TABLE eclipse ("fecha" DATETIME,  "tipo" VARCHAR ,"hora" VARCHAR,"signo_imagen" VARCHAR, "signo_descripcion" VARCHAR, "id_eclipse" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );');
-		alert("se creo la tabla ddd");
+		tx.executeSql( 'CREATE TABLE decreto ("fecha" DATETIME,  "decreto" VARCHAR , "id_decreto" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );');
+		tx.executeSql( 'CREATE TABLE lfc ("fecha" DATETIME,  "lfc_inicio" VARCHAR , "lfc_fin" VARCHAR , "id_lfc" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE );');
+		
+		//alert("se creo la tabla ddd");
         tx.executeSql('INSERT INTO prueba (fecha, sol_signo,horario_verano,estacion, aspecto_astrologico,luna_signo,decreto) VALUES ("13-12-2013","sagitario","si","otoño","una prueba","capricornio","valla al concierto")'  );
 		tx.executeSql('INSERT INTO prueba (fecha, sol_signo,horario_verano,estacion, aspecto_astrologico,luna_signo,decreto) VALUES ("15-12-2013","capricornio","no","invierno","una prueba dos","escorpion","america bicampeon")'  );
 		tx.executeSql('INSERT INTO prueba (fecha, sol_signo,horario_verano,estacion, aspecto_astrologico,luna_signo,decreto) VALUES ("16-12-2013","acuario","si","otoño","una prueba tres","picis","america subcampeon")'  );
 		
-		tx.executeSql('INSERT INTO fase_lunar (fecha_inicio, fecha_fin, nombre_fase, signo_imagen, signo_descripcion, aspecto_astrologico,num_lunacion) VALUES ("05-12-2013","16-12-2013", "Luna creciente","2","tauro","prueba 1", "1");'  );
-		tx.executeSql('INSERT INTO fase_lunar (fecha_inicio, fecha_fin, nombre_fase, signo_imagen, signo_descripcion, aspecto_astrologico,num_lunacion) VALUES ("17-12-2013","31-12-2013", "Luna nueva","3","geminis","prueba 2, "2")';  );
+		tx.executeSql('INSERT INTO fase_lunar (fecha_inicio, fecha_fin, nombre_fase, signo_imagen, signo_descripcion, aspecto_astrologico,num_lunacion) VALUES ("05-12-2013","18-12-2013", "Luna creciente","2","tauro","prueba 1", "1");'  );
+//		tx.executeSql('INSERT INTO fase_lunar (fecha_inicio, fecha_fin, nombre_fase, signo_imagen, signo_descripcion, aspecto_astrologico,num_lunacion) VALUES ("17-12-2013","31-12-2013", "Luna nueva","3","geminis","prueba 2, "2")' );
 		console.log("se va a insertar eclipse");
-		tx.executeSql('INSERT INTO eclipse (fecha, tipo, hora, signo_imagen, signo_descripcion) VALUES ("16-12-2013","Lunar", "4hrs", "3","geminis")'  );
+		tx.executeSql('INSERT INTO eclipse (fecha, tipo, hora, signo_imagen, signo_descripcion) VALUES ("17-12-2013","Lunar", "4hrs", "3","geminis")'  );
 		console.log("primer registro");
-		tx.executeSql('INSERT INTO eclipse (fecha, tipo, hora, signo_imagen, signo_descripcion) VALUES ("17-12-2013","Solar", "5hrs","2","tauro")'  );
+		tx.executeSql('INSERT INTO eclipse (fecha, tipo, hora, signo_imagen, signo_descripcion) VALUES ("18-12-2013","Solar", "5hrs","2","tauro")'  );
+		tx.executeSql('INSERT INTO eclipse (fecha, tipo, hora, signo_imagen, signo_descripcion) VALUES ("19-12-2013","Lunar", "9hrs","3","geminis")'  );
 		console.log("segundo registro");
+		
+		tx.executeSql('INSERT INTO decreto (fecha, decreto) VALUES ("17-12-2013","Hoy sera un buen dia")'  );
+		tx.executeSql('INSERT INTO decreto (fecha, decreto) VALUES ("18-12-2013","Mañana sera mejor")'  );
+		tx.executeSql('INSERT INTO decreto (fecha, decreto) VALUES ("19-12-2013","Un buen dia para negociar")'  );
+		
+		
+		tx.executeSql('INSERT INTO lfc (fecha, lfc_inicio, lfc_fin) VALUES ("17-12-2013","0:22","10:37")'  );
+		tx.executeSql('INSERT INTO lfc (fecha, lfc_inicio, lfc_fin) VALUES ("18-12-2013","6:02","12:30")'  );
+		tx.executeSql('INSERT INTO lfc (fecha, lfc_inicio, lfc_fin) VALUES ("19-12-2013","13:22","19:09")'  );
+		
+		
+		
+		
 		
         //tx.executeSql('INSERT INTO SoccerPlayer(Name,Club) VALUES ("Van Persie", "Arsenal")');
     }
@@ -48,18 +67,23 @@ document.addEventListener("deviceready", onDeviceReady, false);
  
     //select all from SoccerPlayer
     function queryFaseLunar(tx){
-    	
-        tx.executeSql('SELECT * FROM fase_lunar where ? BETWEEN fecha_inicio AND fecha_fin ',[hoy],SuccessFaseLunar,errorCB);
-        
+        tx.executeSql('SELECT * FROM fase_lunar where ? BETWEEN fecha_inicio AND fecha_fin ',[hoy],SuccessFaseLunar,errorCB);        
     }
 	
 	
 	function queryEclipse(tx){
-    	
         tx.executeSql('SELECT * FROM eclipse where fecha= ?',[hoy],SuccessEclipse,errorCB);
-        
     }
- 
+	
+	
+	function queryDecreto(tx){
+        tx.executeSql('SELECT * FROM decreto where fecha= ?',[hoy],SuccessDecreto,errorCB);
+    }
+	
+	function queryLfc(tx){
+        tx.executeSql('SELECT * FROM lfc where fecha= ?',[hoy],SuccessLfc,errorCB);
+    }
+	
     function SuccessFaseLunar(tx,result){
     	
         //$('#SoccerPlayerList').empty();
@@ -74,19 +98,23 @@ document.addEventListener("deviceready", onDeviceReady, false);
 				//alert(result.rows.item(0)['estacion']);
         		//alert(result.rows.item(1)['Name']);
 			//	alert("correct");
-			if (typeof result.rows.item(0) != 'undefined') {
+			if (result.rows.item(0)) {
 			   var row = result.rows.item(0);
 				//$("#sol_signo").html(row['sol_signo']);
 				//$("#estacion").html(row['estacion']);
 				$("#detalle_faseLunar").html( row['nombre_fase'] + " "+ row['signo_descripcion']);
 				$("#aspecto_astrologico").html( row['aspecto_astrologico']);
-				$("#imagen_signo").removeClass().addClass("z-"+ row['signo_imagen'] +" top");
+				$("#imagen_signo").removeClass().addClass("z-"+ row['signo_imagen'] +" position-fixed");
+				$("#fase_lunar").html( row['nombre_fase'] + " en "+ row['signo_descripcion']);
+				$("#sol_signo").html("Sol en signo: "+ row['signo_descripcion']);
 				//$("#imagen_signo").remove;
             i++;
 			}else{
 				$("#detalle_faseLunar").html();
 				$("#aspecto_astrologico").html();
 				$("#imagen_signo").removeClass();
+				$("#fase_lunar").html();
+				$("#sol_signo").html();
 			}
            
             //$('#SoccerPlayerList').append('<li><a href="#"><h3 class="ui-li-heading">'+row['Name']+'</h3><p class="ui-li-desc">Club '+row['Club']+'</p></a></li>');
@@ -97,12 +125,34 @@ document.addEventListener("deviceready", onDeviceReady, false);
 	
 	
 	
-	function SuccessEclipse(tx,result){
-    	
-       
+	function SuccessEclipse(tx,result){     
 			if (typeof result.rows.item(0) != 'undefined') {
+				var row = result.rows.item(0);
+				//alert( row['tipo'] );
 				$("#eclipse").html( row['tipo'] );
 			}else{
 				$("#eclipse").html();
+			}
+    }
+	
+	
+	
+	function SuccessDecreto(tx,result){
+			if (typeof result.rows.item(0) != 'undefined') {
+				var row = result.rows.item(0);
+				$("#decreto").html( row['decreto'] );
+			}else{
+				$("#decreto").html();
+			}
+    }
+	
+	
+	function SuccessLfc(tx,result){
+			if (typeof result.rows.item(0) != 'undefined') {
+				//alert("LFC");
+				var row = result.rows.item(0);
+				$("#lfc").html("LFC "+ row['lfc_inicio'] +" - "+ row['lfc_fin']);
+			}else{
+				$("#lfc").html();
 			}
     }
